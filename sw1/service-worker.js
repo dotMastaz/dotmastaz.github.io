cacec6c228708412e607b7d8ba024ef4fb0e283c
Nicolas <dotmastaz@gmail.com>
2015-05-03 22:15:39 +0200

    SW1

--- sw1/service-worker.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js', {scope: './'}).then(function(registration) {
-        document.querySelector('#service-worker-registration').textContent = '✓ Success';
-    }).catch(function(error) {
-        document.querySelector('#service-worker-registration').textContent = '✘ Error:' + error;
-    });
-} else {
-    document.querySelector('#service-worker-registration').textContent = '✘ Unavailable!'
-}
